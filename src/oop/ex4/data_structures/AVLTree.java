338f6d16bd953c6ee3681caed6d7dc6ebc4a11d2
TomerGreen <tomer.greenberg@gmail.com>
2018-05-15 12:23:29 +0300

    added AVLTreeTest

--- src/oop/ex4/data_structures/AVLTree.java
+++ src/oop/ex4/data_structures/AVLTree.java
@@ -5,4 +5,11 @@ package oop.ex4.data_structures;
  * A class representing an AVL tree.
  */
 public class AVLTree {
+
+    /** The root node. */
+    protected Node root;
+
+    public int contains(int searchVal) {
+        return -1;
+    }
 }
---
+++ src/oop/ex4/data_structures/AVLTreeTest.java
@@ -0,0 +1,7 @@
+package oop.ex4.data_structures;
+
+import static org.junit.Assert.*;
+
+public class AVLTreeTest {
+
+}
