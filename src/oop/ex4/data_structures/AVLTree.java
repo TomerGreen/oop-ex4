4fcb4b06e731cb8a7fb8754aebb0d128cd0144de
idobaril <39239875+idobaril@users.noreply.github.com>
2018-05-15 12:19:41 +0300

    Added value to Node
    Added value to Node

--- src/oop/ex4/data_structures/AVLTree.java
+++ src/oop/ex4/data_structures/AVLTree.java
@@ -5,4 +5,24 @@ package oop.ex4.data_structures;
  * A class representing an AVL tree.
  */
 public class AVLTree {
+
+    public boolean add(int newValue){
+        if(contains(newValue) > 0){
+            Node father = getLocation(newValue);
+
+
+        }
+        return false; //newValue is contained in the tree.
+    }
+
+
+    private Node getLocation(int newValue){
+        return new Node(1);
+    }
+
+    private int contains(int searchVal){
+        return 1;
+    }
+
+
 }
--- src/oop/ex4/data_structures/NodeTest.java
+++ src/oop/ex4/data_structures/NodeTest.java
@@ -9,12 +9,14 @@ public class NodeTest {
 
     @Test
     public void createNode(){
-        Node root = new Node();
-        Node leaf = new Node(root);
+        Node root = new Node(2);
+        Node leaf = new Node(root, 9);
         assertEquals("leaf's father is not right", leaf.getFather(), root);
         assertEquals("Root has a father", root.getFather(), null);
         assertTrue("Leaf has children", leaf.getLeftSon() == null && leaf.getRightSon() == null);
         assertEquals("Leaf's height isn't zero", leaf.getHeight(), 0);
+        assertTrue("Value isn't right", root.getValue() ==2 & leaf.getValue() == 9 );
+
 
     }
 
