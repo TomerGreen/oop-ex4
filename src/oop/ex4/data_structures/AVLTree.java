1b24c6bd99754449ff8d8729e9edb46f9f11ddbe
TomerGreen <tomer.greenberg@gmail.com>
2018-05-15 14:40:50 +0300

    added my functions

--- src/oop/ex4/data_structures/AVLTree.java
+++ src/oop/ex4/data_structures/AVLTree.java
@@ -5,7 +5,34 @@ package oop.ex4.data_structures;
  * A class representing an AVL tree.
  */
 public class AvlTree {
-    Node root;
+
+    /** The root node. */
+    protected Node root;
+
+    /**
+     * Creates an empty tree.
+     */
+    public AvlTree() {
+        root = null;
+    }
+
+    /**
+     * Creates a new tree by adding values from an array, one by one.
+     * @param data The array of values that
+     */
+    public AvlTree(int[] data) {
+        for (int value : data) {
+            this.add(value);
+        }
+    }
+
+    /**
+     * A copy constructor that creates a new tree from an existing one.
+     * @param tree The copied tree.
+     */
+    public AvlTree(AvlTree tree) {
+        this.root = tree.root;
+    }
 
     public boolean add(int newValue){
         Node father = getLocation(newValue);
@@ -88,8 +115,27 @@ public class AvlTree {
 
     }
 
+    /**
+     * Given a new value to be added, returns the expected father node, before any rotation.
+     * @param newValue The value to be added.
+     * @return The expected father node if newValue is not in the tree, null otherwise.
+     */
     private Node getLocation(int newValue){
-        return new Node(1);
+        Node currNode = root;  // The node we're comparing too, and might be returned.
+        Node nextNode = root;  // The node we proceed to, or stop if it's null.
+        while (nextNode != null) {
+            currNode = nextNode;
+            if (newValue == currNode.getValue()) {
+                return null;
+            }
+            else if (newValue > currNode.getValue()) {
+                nextNode = currNode.getRightSon();
+            }
+            else {
+                nextNode = currNode.getLeftSon();
+            }
+        }
+        return currNode;
     }
 
     private int contains(int searchVal){
