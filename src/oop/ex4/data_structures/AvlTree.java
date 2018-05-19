5413105c91102ebdb4578542d8ddfc9cbdbcb737
TomerGreen <tomer.greenberg@gmail.com>
2018-05-19 13:59:24 +0300

    AvlTree now inherits from abstract class BinarySearchTree

--- src/oop/ex4/data_structures/AvlTree.java
+++ src/oop/ex4/data_structures/AvlTree.java
@@ -7,13 +7,7 @@ import static java.lang.Math.max;
 /**
  * A class representing an AVL tree.
  */
-public class AvlTree implements Iterable<Integer> {
-
-    /* The number of nodes in the tree */
-    private int size;
-
-    /** The root node. */
-    private Node root;
+public class AvlTree extends BinarySearchTree {
 
     /**
      * Creates an empty tree.
@@ -78,20 +72,6 @@ public class AvlTree implements Iterable<Integer> {
         return 1 + findMinNodes(h-1) + findMinNodes(h-2);
     }
 
-    /**
-     * returns the number of nodes in the tree.
-     * @return The nunmber of nodes in the tree.
-     */
-    public int size(){
-        return size;
-    }
-
-    /**
-     * Add a new node with the given key to the tree.
-     * @param newValue the value of the new node to add.
-     * @return true if the value to add is not already in the tree and it was successfully added,
-     * false otherwise.
-     */
     public boolean add(int newValue){
         Node father = expectedParent(newValue);
         if(father != null){
@@ -306,11 +286,6 @@ public class AvlTree implements Iterable<Integer> {
         }
     }
 
-    /**
-     * Removes the node with the given value from the tree, if it exists.
-     * @param toDelete The value to be deleted.
-     * @return True if was successfully deleted, false otherwise.
-     */
     public boolean delete(int toDelete){
         Node deleteNode = valueToNode(toDelete);
         if (deleteNode.getValue() != toDelete){
@@ -404,11 +379,6 @@ public class AvlTree implements Iterable<Integer> {
         return null;
     }
 
-     /**
-     * Returns whether a value is in the tree.
-     * @param searchVal The value to be searched.
-     * @return The depth of the node with that value, or -1 if it does not exist.
-     */
     public int contains(int searchVal){
         Node candidate = valueToNode(searchVal);
         if (candidate == null || candidate.getValue() != searchVal) {
@@ -417,68 +387,6 @@ public class AvlTree implements Iterable<Integer> {
         else {
             return candidate.getHeight();
         }
-    }
-
-
-    /**
-     * @param searchVal The value to be searched.
-     * @return The expected parent node of the value if it is not in the tree, and null otherwise.
-     */
-    private Node expectedParent(int searchVal) {
-        Node candidate = valueToNode(searchVal);
-        if (candidate == null || searchVal == candidate.getValue()) {
-            return null;
-        }
-        return candidate;
-    }
-
-    /**
-     * Returns a node with a given value if it exists, or the node that is expected to be its
-     * parent if it was added to the tree (prior to rotations).
-     * This helper method is used by several API methods to prevent probing the tree multiple times.
-     * @param searchVal The value to be searched.
-     * @return The expected father node if searchValue is not in the tree, or the node with the same value.
-     * Returns null only if the tree is empty.
-     */
-    private Node valueToNode(int searchVal){
-        Node currNode = root;  // The node we're comparing to, and might be returned.
-        Node nextNode = root;  // The node we proceed to, or stop if it's null.
-        while (nextNode != null) {
-            currNode = nextNode;
-            if (searchVal == currNode.getValue()) {
-                return currNode;
-            }
-            else if (searchVal > currNode.getValue()) {
-                nextNode = currNode.getRightSon();
-            }
-            else {
-                nextNode = currNode.getLeftSon();
-            }
-        }
-        return currNode;
-    }
-
-    /**
-     * Returns an ordered list of the values in the tree.
-     * @return An ascending order integer list of values in the tree.
-     */
-    private LinkedList<Integer> getTreeList() {
-        LinkedList<Integer> list = new LinkedList<>();
-        Node currNode = root.getSubtreeMinNode();
-        while (currNode != null) {
-            list.add(currNode.getValue());
-            currNode = currNode.getSuccessor();
-        }
-        return list;
-    }
-
-    /**
-     * Returns an iterator for the AVL tree. The iterator
-     * @return
-     */
-    public Iterator<Integer> iterator() {
-        LinkedList<Integer> treeList = getTreeList();
-        return treeList.iterator();
     }
 
 }
